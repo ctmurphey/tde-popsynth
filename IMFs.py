4c7faea4b6cba8d342285050c20997daa48028da
Tanner Murphey <ctmurphey@protonmail.com>
2023-08-20 05:21:59 -0500

    created IMF library

---
+++ IMFs.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+'''Collection of the various IMF functions for stars and SMBHs as
+well as the necessary functions to generate the populations'''
+
+### Stars
+
+
+
+### SMBH
+
+
+
+### Synthesizing population
